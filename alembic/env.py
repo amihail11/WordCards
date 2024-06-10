c7e09e5bd6e6876dc4178a02e87b17a98eab26ff
amihail11 <ar.mihail89@gmail.com>
2024-06-10 20:53:55 +0700

    Add a missing migration. Fix models for alembic #1

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from config import DATABASE_URL
 from wordcards.database import Base
+from wordcards.models import *  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
